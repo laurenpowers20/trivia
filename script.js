4ea96c1345675798c542fba441467f577f991888
Lauren Powers <laurenpowers@Laurens-MacBook-Air.local>
2022-10-04 09:44:23 -0400

    re-did startGame() to make it more clean

--- script.js
+++ script.js
@@ -1,14 +1,12 @@
-let startButton = document.querySelector(".start-button");
-let gameContainer = document.querySelectorAll(".game-container");
+let startButton = document.querySelector("#start-button");
+let questionContainer = document.querySelector("#question-container");
+let nextButton = document.querySelector("#next-button");
 
-startButton.addEventListener("click", newQuestion);
+startButton.addEventListener("click", startGame);
 
-function newQuestion() {
-  const questionDiv = document.createElement("div");
-  questionDiv.innerHTML = `<h3>Which president is on the $2 bill?</h3>
-  <p>George Washington</p> <p>Thomas Jefferson</p><p>James Monroe</p><p>Grover Cleveland</p>`;
-  document.body.append(questionDiv);
-  document.querySelector(".intro").style.display = "none";
+function startGame() {
+  questionContainer.classList.remove("hide");
+  startButton.classList.add("hide");
 }
 
 // 1. Player enters game by clicking on start button
@@ -16,3 +14,15 @@ function newQuestion() {
 // 3. Once choice is made, next question populates
 // 4. Score is stored
 // 5. Percentage of score will be shown on last div visible.
+
+const triviaQuestions = [
+  {
+    question: "Which president is on the $2 dollar bill?",
+    answers: [
+      { text: "George Washington", correct: false },
+      { text: "Thomas Jefferson", correct: true },
+      { text: "James Monroe", correct: false },
+      { text: "Grover Cleveland", correct: false },
+    ],
+  },
+];
